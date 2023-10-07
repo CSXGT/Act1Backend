2194c53abd02fa41ba26e090a7311b602439dc67
Cris <cristhofer2023solarte@gmail.com>
2023-10-06 21:00:47 -0400

    guardado

--- routes/equipos.js
+++ routes/equipos.js
@@ -1,1 +1,9 @@
-console.log("hola mundo")
+var express = require('express');
+var router = express.Router();
+
+/* GET equipos listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
