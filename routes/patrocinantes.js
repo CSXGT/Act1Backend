48a7f1df03e31a73e3100b92bae32a1cdfd13b53
Cris <cristhofer2023solarte@gmail.com>
2023-10-06 21:41:10 -0400

    guardado.

--- routes/patrocinantes.js
+++ routes/patrocinantes.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+import { PatrocinantesController } from "../controllers/patrocinantes.c"
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(PatrocinantesController.todos());
 });
 
 module.exports = router;
